216fa04fe87579effa9bd1b777c8deeba3812843
abdullah s. canipek <ascanipek@gmail.com>
2021-02-11 17:34:00 +0300

    git başlatıldı

---
+++ js/my.js
@@ -0,0 +1,19 @@
+let degisken = "Merhaba Eren"
+
+// Aritmatiksel Operatörler
+//  + , - , * , /, % 
+// Atama Operatörleri
+//  =, +=, -=, *=, /=, %=
+// Karşılaştırma Operatörleri
+//  ==, !=, ===, <, <=, >, >=
+
+let bir = '13'
+let iki = 13
+
+// console.log(bir === iki)
+
+// ve (&&), veya (||), değil !
+
+console.log(((1 == 1) || (1 > 2)) && !(1 > 10))
+
+
