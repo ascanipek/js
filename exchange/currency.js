bc59cff7a9d59ad14f8875c35a9e4d22dd28ca29
abdullah s. canipek <ascanipek@gmail.com>
2021-03-14 21:12:38 +0300

    currency app

---
+++ exchange/currency.js
@@ -0,0 +1,59 @@
+let rates = null
+let amount = document.querySelector('#amount')
+let firstCurr = document.querySelector('#firstCurrency')
+let secondCurr = document.querySelector('#secondCurrency')
+
+function eventListeners(){
+    // inputa her giriş yapıldığında tetiklenecek
+    amount.addEventListener("input", hesapla)
+
+    // birinci select değiştiğinde tettiklenecek
+    firstCurr.onchange = function(){
+        getData(this.value)
+        .then(data => {
+            rates = data
+            hesapla()
+            document.getElementById('outputFirst').innerHTML = this.value
+        })
+        
+    }
+
+    // ikinci select değiştiğinde tettiklenecek
+    secondCurr.onchange = function(){
+        hesapla()
+        document.getElementById('outputSecond').innerHTML = this.value
+    }
+
+    // döküman tamamen yüklendiğinde 
+    document.addEventListener("DOMContentLoaded", function(event){
+        getData(firstCurr.value)
+        .then(res => {
+            console.log(res)
+            rates = res
+        })
+    })
+}
+
+// olay dinleyici fonksiyon en az bir kez çalışıyor
+eventListeners()
+
+
+// veriyi servisten çeken fonksiyon
+async function getData(base){
+    let promise = new Promise((resolve, reject) => {
+        fetch("https://api.exchangeratesapi.io/latest?base=" + base )
+        .then(response => response.json())
+        .then(data => { resolve(data.rates)})
+    })
+    const sonuc = await promise
+    return sonuc
+}
+
+
+// kullanıcın girdiği değer ve seçtiği kur bilgilerine göre hesap yaparak sonucu yazan fonksiyon
+function hesapla(){
+    let calculated = amount.value * rates[secondCurr.value]
+    document.querySelector('#outputResult').value = calculated.toFixed(2)
+}
+
+
